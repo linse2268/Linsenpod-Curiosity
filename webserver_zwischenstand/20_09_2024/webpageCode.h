fbb8cdb87b446b776c27008f68d0ef0f3979d3d7
SquidPod <159013962+SquidPod@users.noreply.github.com>
2024-09-20 14:12:05 +0200

    Add files via upload

---
+++ webserver_zwischenstand/20_09_2024/webpageCode.h
@@ -0,0 +1,33 @@
+const char webpagecode[] = 
+R"=====(
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        button{
+            border: none;
+            border-radius: 1vw;
+            width: 30vw;
+            height: 50vh;
+            font-size: 3vw;
+        }
+        #blue{
+            background-color: red;
+        }
+        #red{
+            background-color: blue;
+            
+        }
+    </style>
+</head>
+<body>
+    <button id="red">red button</button>
+    <button id="blue">blue button</button>
+</body>
+</html>
+)=====";
+
+
