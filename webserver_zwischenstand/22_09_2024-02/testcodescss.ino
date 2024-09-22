588ba3dad1009af735ce6c7a3d402241e9e301c4
SquidPod <159013962+SquidPod@users.noreply.github.com>
2024-09-22 18:40:50 +0200

    Update testcodescss.ino

--- webserver_zwischenstand/22_09_2024-02/testcodescss.ino
+++ webserver_zwischenstand/22_09_2024-02/testcodescss.ino
@@ -6,7 +6,7 @@
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
-//#include "webpageCode.h"
+#include "webpageCode.h"
 
 const char* ssid = "ringleben";
 const char* password = "theahermine";
